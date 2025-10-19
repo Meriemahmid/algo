8c4d37a5ccf72f1fff0d3151ea7bb2d84d94c4a8
Meriem Ahmid <meriemahmid6@gmail.com>
2025-10-19 16:03:23 +0200

    Update tp1.c

--- tp1.c
+++ tp1.c
@@ -1,2 +1,28 @@
-bjbjbjbjjbj
-,;,;,;,;,;,;mlmlm
+#include <stdio.h>
+int main () { 
+ int n;
+   printf(" give the dimensions of the matrix:"); scanf("%d",&n);
+
+int tab[n][n], arr[n][n];
+
+   printf(" give the matrix eiements: ");
+
+      for(int i = 0; i < n; i++) {
+        
+        for( int j = 0; j < n; j++) {
+          
+           scanf("%d", tab[i][j]
+             }
+      }  // reading the matrix
+
+   for(int i = 0; i < n; i++) {
+
+     for(int j = 0; i < n; i++){
+       
+       arr[i][j] = tab[j][i]; 
+       
+     }
+   }
+
+for(int i =0; 
+       
